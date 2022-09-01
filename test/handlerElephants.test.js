0196873867e9cc1dae1c7f668f22af02520bb841
Joao Victor <imashinzo10@gmail.com>
2022-09-01 19:51:37 -0300

    Requisito 7 iniciado falta lint e testes, requisitos anteriores testados e com lint corrigido.

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,31 @@
 const handlerElephants = require('../src/handlerElephants');
+const { species } = require('../data/zoo_data');
+const values = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+const elephants = species.find((specie) => specie.name === 'elephants');
+const mapElefants = elephants.residents.map((elephant) => elephant.name);
+
+describe('Testes da função HandlerElephants', () => {
+  it('Parametro indefinido retorna undefined', () => {
+    expect(handlerElephants(undefined)).toEqual(undefined);
+  });
+  it('Parâmetro inválido, uma string e necessaria', () => {
+    expect(handlerElephants(5)).toEqual('Parâmetro inválido, uma string e necessaria');
+  });
+  it('Numero de Elefantes Residentes', () => {
+    expect(values('count')).toEqual(elephants.residents.length);
+  });
+  it('Nomes dos Elefantes', () => {
+    expect(values('names')).toEqual(mapElefants);
+  });
+  it('Idade media', () => {
+    const averageAge = ({ residents }) => residents.reduce((sum, elephant) => sum + elephant.age, 0) / residents.length;
+    expect(values('averageAge')).toEqual(averageAge(elephants));
+  });
+  it('Valor - default', () => {
+    expect(values('default')).toEqual(null);
+  });
+  it('Popularidade ser igual a 5', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+});

2579041b1bccfed9a996dcf13b56107a314852f8
Joao Victor <imashinzo10@gmail.com>
2022-09-01 20:45:23 -0300

    Requisito 7 e 8 iniciados erros de lint, falta testes

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((criancas) => criancas.age < 18).length;
+  const adult = entrants.filter((adultos) => adultos.age >= 18 && adultos.age < 50).length;
+  const senior = entrants.filter((idosos) => idosos.age >= 50).length;
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const entrantsValue = countEntrants(entrants);
+  const valueChild = entrantsValue.child * data.prices.child;
+  const valueAdult = entrantsValue.adult * data.prices.adult;
+  const valueSenior = entrantsValue.senior * data.prices.senior;
+  return valueChild + valueAdult + valueSenior;
 }
 
 module.exports = { calculateEntry, countEntrants };

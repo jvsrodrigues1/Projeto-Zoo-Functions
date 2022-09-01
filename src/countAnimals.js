9b7b4e4bd09cef3e88cdcff8f2152c5514aa7fb7
Joao Victor <imashinzo10@gmail.com>
2022-09-01 19:01:30 -0300

    Requisito 4 e 5 finalizados

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+  if (!animal.sex) {
+    return species.find((specie) => specie.name === animal.specie).residents.length;
+  }
+  const zoo = species.find((specie) => specie.name === animal.specie);
+  return zoo.residents.filter((resident) => resident.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;

e414489d8b4cbb0d8f8f7ad2d841287dde67383e
Joao Victor <imashinzo10@gmail.com>
2022-09-02 11:53:02 -0300

    Requisito 8 finalizado e execucao dos testes openingHours

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstSpecie = employees.find((employees2) => employees2.id === id).responsibleFor[0];
+  const animals = data.species.find((species) => species.id === firstSpecie).residents;
+  const oldest = animals.reduce((acc, curr) => (acc.age > curr.age ? acc : curr), []);
+  return Object.values(oldest);
 }
 
 module.exports = getOldestFromFirstSpecies;
--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,54 @@
 const getOpeningHours = require('../src/getOpeningHours');
+const validateHour = require('../src/getOpeningHours');
+const validateDay = require('../src/getOpeningHours');
+const { hours } = require('../data/zoo_data');
+const validateAbbreviation = require('../src/getOpeningHours');
+const isStringRepresentNumber = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Verificacao AM e PM', () => {
+    expect(() => (!['AM', 'PM'].includes('AM', 'PM') === true).toThrow(Error));
+  });
+  it('Verificacao hora entre 0 e 12', () => {
+    expect(() => { getOpeningHours('Monday', '13:00-AM'); }).toThrow('The hour must be between 0 and 12');
+  });
+  it('Teste verificacao de horas', () => {
+    expect(() => (validateHour('15:13')).toThrow(Error));
+  });
+  it('Verificacao minuto valido entre 0 e 59', () => {
+    expect(() => { getOpeningHours('Monday', '12:70-AM'); }).toThrow('The minutes must be between 0 and 59');
+  });
+  it('Teste Validacao de minutos', () => {
+    expect(() => (validateHour('15:80')).toThrow(Error));
+  });
+  it('Verificao para diga valido', () => {
+    expect(() => (validateDay('Monday')).toThrow(Error));
+  });
+  it('Verificacao teste vazio', () => {
+    expect(getOpeningHours('', '')).toEqual(hours);
+  });
+  it('Teste validacao de abreviacao ', () => {
+    expect(() => validateAbbreviation('AP', 'PA')).toThrow(Error);
+  });
+  it('Teste de abreviacao AM ou PM', () => {
+    expect(() => getOpeningHours('Tuesday', '09:00-CM')).toThrow('The abbreviation must be \'AM\' or \'PM\'');
+  });
+  it('Testando abreviacao ', () => {
+    expect(() => validateAbbreviation('AM', 'PM')).toBeTruthy();
+  });
+  it('Aberto ou fechado teste 1', () => {
+    expect(getOpeningHours('Monday', '9:00-AM')).toBeTruthy();
+  });
+  it('Aberto ou fechado teste 2', () => {
+    expect(getOpeningHours('Tuesday', '9:00-PM')).toBeTruthy();
+  });
+  it('Aberto ou fechado teste 3', () => {
+    expect(getOpeningHours('Wednesday', '10:00-PM')).toBeTruthy();
+  });
+  it('Teste de numbers ou string', () => {
+    expect(() => isStringRepresentNumber('9', '9')).toThrow(Error);
+  });
+  it('Verificacao mensagem aberto', () => {
+    expect(getOpeningHours('Wednesday', '10:00-AM')).toEqual('The zoo is open');
+  });
+});
